f6d7a0f04a8b7c540ebf8e88b3dc18ead44ec07a
apanova22 <panova-anastasiya@inbox.ru>
2018-07-12 20:49:58 +0600

    Начата 14 задача

---
+++ Лабораторная 5/Задача 14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leftA = int.Parse(Console.ReadLine());
+            int leftB = int.Parse(Console.ReadLine());
+            int rightA = int.Parse(Console.ReadLine());
+            int rightB = int.Parse(Console.ReadLine());
+            if (leftA < leftB && rightA < rightB)
+            {
+                if (leftA < rightA)
+                {
+                    if (leftB < rightA)
+                    {
+                        Console.WriteLine("Не пересекаются");
+                    }
+                    if (leftB > rightA && leftB < rightB)
+                    {
+                        Console.WriteLine("Пересекаются");
+                    }
+                    if (leftB > rightB)
+                    { Console.WriteLine("Пересекаются"); }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Значение А должно быть меньше В");
+            }
+        }
+    }
+}
